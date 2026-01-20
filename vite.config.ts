fb3290042c5837f87cab99afdb4b12ea2e16d678
AdrianPerez221 <aperez@student.ifp.es>
2026-01-20 11:41:24 +0100

    g

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "node:url";
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   base: mode === "github" ? "/vite-app/" : "/",
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 }));
