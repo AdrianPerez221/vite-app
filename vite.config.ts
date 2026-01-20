481df234342e5aced8fcf89161a1addd3e25f87d
AdrianPerez221 <aperez@student.ifp.es>
2026-01-20 10:59:49 +0100

    g

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const base =
-    mode === "github" ? "/vite-app/" : "/"; // <- tu repo: vite-app
-
-  return {
-    plugins: [react()],
-    base,
-  };
-});
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  base: mode === "github" ? "/vite-app/" : "/",
+}));
